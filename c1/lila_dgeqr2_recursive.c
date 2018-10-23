79ee2190542919e9a7f9e24884dcff44bf741a0c
Julien Langou <julien.langou@ucdenver.edu>
2018-10-23 17:53:54 +0800

    some edits in the code:
    Look at the commit:
    (main point is #3)
    
    1) I am not sure what is ceil() in C. If you do x / y where x and y are int,
    then the results is an int, and this is int is ``floor(x/y)`` in matlab. So I
    changed your ceil(x/y) by x/y.  You need to understand this. ``x/y`` in C takes
    with two (int)'s returns an (int) and this is (int) is ``floor(x/y)``.
    
    2) I went all the way down to n = 1 in the recursion.
    What I would like you to do now, is to have two leaves.
    One leaf is for n < 10 say, and then you call an unblocked code like
    	info = lila_dgeqr2_w02b( m, n, i, mt, A, lda, T, ldt, Q, ldq, work, lwork );
    fine. The leaf for n < 2 (so n = 1), needs to be done by hand.  ``All`` you
    need to do is construct V, Q and tau. (You have two tau: one in T and one in
    TTT)
    
    3) I am using two recursions, one on the left, one on the right, you were only
    using recursion on the right. I am not sure why.

--- c1/lila_dgeqr2_recursive.c
+++ c1/lila_dgeqr2_recursive.c
@@ -4,20 +4,23 @@ int lila_dgeqr2_recursive( int m, int n, int i, int mt, double *A, int lda, doub
 
 	int info, nb1, nb2; 
 
-	nb1 = ceil(n/2);
-	nb2 = n - nb1;
-	
-	printf(" i = %d, nb1 = %d, nb2 = %d, n = %d,\n", i,nb1,nb2,n);
 
-	if ( n < 10 ) { 
+	if ( n < 2 ) {
+
+		printf(" i = %d, (leaf) n = %d,\n", i,n);
 
 //		info = lila_dge_qr_wq_w03( m, n, i, mt, A, lda, T, ldt, TTT, llldddttt, Q, ldq, work, lwork );
 		info = lila_dgeqr2_w02b( m, n, i, mt, A, lda, T, ldt, Q, ldq, work, lwork ); // This function doesn't have TTT llldddttt in the interface
 
 	} else {
 
+	nb1 = n / 2;
+	nb2 = n - nb1;
+	printf(" i = %d, nb1 = %d, nb2 = %d, n = %d,\n", i,nb1,nb2,n);
+
 //	info = lila_dge_qr_wq_w03( m, nb1, i, mt, A, lda, T, ldt, TTT, llldddttt, Q, ldq, work, lwork );
-	info = lila_dgeqr2_w02b( m, nb1, i, mt, A, lda, T, ldt, Q, ldq, work, lwork ); // This function doesn't have TTT llldddttt in the interface
+//	info = lila_dgeqr2_w02b( m, nb1, i, mt, A, lda, T, ldt, Q, ldq, work, lwork ); // This function doesn't have TTT llldddttt in the interface
+	info = lila_dgeqr2_recursive( m, nb1, i, mt, A, lda, T, ldt, TTT, llldddttt, Q, ldq, work, lwork );
 
 	info = lila_dge_qr_ormqrf_w03( m, nb2, nb1, i, i+nb1, mt, A, lda, T, ldt, TTT, llldddttt, work, lwork );
 
