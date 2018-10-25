0e3d45c75572fb80a21b291ad62887002606b03c
Julien Langou <julien.langou@ucdenver.edu>
2018-10-25 15:57:32 +0800

    this code is now working with the direct call to BLAS to simulate LARFB

--- c1/lila_dge_qr_ormqrbz_w03.c
+++ c1/lila_dge_qr_ormqrbz_w03.c
@@ -9,28 +9,27 @@ int lila_dge_qr_ormqrbz_w03( int m, int n, int k, int i, int j, int mt, double *
 
 	vb = ((i+k) % mt); if (vb == 0) vb = mt; if ( vb > k ) vb = k;
 
-//	This is the logic that follows the code in matlab
 	Aii = A + (i+k-vb) + (i+k-vb)*lda; 
 	Qij = Q + (i+k-vb) + j*ldq;
 	Tii = T + (i % mt) + (i+k-vb)*ldt;
 
 	ldwork = k;
 
-	ml = m - i - k + vb;
+	ml = m - (i+k-vb);
 	printf("i = %d, k = %d, vb = %d, ml = %d,\n",i,k,vb,ml);
 
 	not_done = 1;
 
 	jj = 1;
 
-//	cblas_dgemm( CblasColMajor, CblasTrans, CblasNoTrans, vb, n, m-i-vb, (1.0e+00), Aii+vb, lda, Qij+vb, lda, (0.0e+00), work, ldwork );
-//	cblas_dtrmm( CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, vb, n, (1.0e+00), Tii, ldt, work, ldwork );
-//	LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', k, vb, work, ldwork, Qij, ldq ); // k, vb    or    vb, vb   ?
-//	cblas_dtrmm( CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, vb, n, (-1.0e+00), Aii, lda, Qij, ldq );
-//	cblas_dgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, m-i-vb, n, vb, (-1.0e+00), Aii+vb, lda, work, ldwork, (1.0e+00), Qij+vb, ldq );
+ 	cblas_dgemm( CblasColMajor, CblasTrans, CblasNoTrans, vb, n, ml-vb, (1.0e+00), Aii+vb, lda, Qij+vb, lda, (0.0e+00), work, ldwork );
+	cblas_dtrmm( CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, vb, n, (1.0e+00), Tii, ldt, work, ldwork );
+	LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', vb, n, work, ldwork, Qij, ldq );
+	cblas_dtrmm( CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, vb, n, (-1.0e+00), Aii, lda, Qij, ldq );
+	cblas_dgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, ml-vb, n, vb, (-1.0e+00), Aii+vb, lda, work, ldwork, (1.0e+00), Qij+vb, ldq );
 
-	info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', vb, n, (0e+00), (0e+00), Qij, ldq );
-	info = LAPACKE_dlarfb_work ( LAPACK_COL_MAJOR, 'L', 'N', 'F', 'C', ml, n, vb, Aii, lda, Tii, ldt, Qij, ldq, work, ldwork );
+//	info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', vb, n, (0e+00), (0e+00), Qij, ldq );
+//	info = LAPACKE_dlarfb_work ( LAPACK_COL_MAJOR, 'L', 'N', 'F', 'C', ml, n, vb, Aii, lda, Tii, ldt, Qij, ldq, work, ldwork );
 
 	while ( not_done == 1 ){
 
@@ -52,18 +51,18 @@ int lila_dge_qr_ormqrbz_w03( int m, int n, int k, int i, int j, int mt, double *
 
 			printf("vb = %d, k = %d\n", vb, k);
 
-			Aii = Aii - vb - vb*lda ;
-			Qij = Qij - vb;
-			Tii = Tii - vb*ldt;
+			Aii -= vb + vb*lda ;
+			Qij -= vb;
+			Tii -= vb*ldt;
 
-//			cblas_dgemm( CblasColMajor, CblasTrans, CblasNoTrans, vb, n, ml, (1.0e+00), Aii+vb, lda, Qij+vb, lda, (0.0e+00), work, ldwork );
-//			cblas_dtrmm( CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, vb, n, (1.0e+00), Tii, ldt, work, ldwork );
-//			LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', k, vb, work, ldwork, Qij, ldq );
-//			cblas_dtrmm( CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, vb, n, (-1.0e+00), Aii, lda, Qij, ldq );
-//			cblas_dgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, ml, n, vb, (-1.0e+00), Aii+vb, lda, work, ldwork, (1.0e+00), Qij+vb, ldq );
+ 			cblas_dgemm( CblasColMajor, CblasTrans, CblasNoTrans, vb, n, ml-vb, (1.0e+00), Aii+vb, lda, Qij+vb, lda, (0.0e+00), work, ldwork );
+			cblas_dtrmm( CblasColMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, vb, n, (1.0e+00), Tii, ldt, work, ldwork );
+			LAPACKE_dlacpy( LAPACK_COL_MAJOR, 'A', vb, n, work, ldwork, Qij, ldq );
+			cblas_dtrmm( CblasColMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, vb, n, (-1.0e+00), Aii, lda, Qij, ldq );
+			cblas_dgemm( CblasColMajor, CblasNoTrans, CblasNoTrans, ml-vb, n, vb, (-1.0e+00), Aii+vb, lda, work, ldwork, (1.0e+00), Qij+vb, ldq );
 
-			info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', vb, n, (0e+00), (0e+00), Qij, ldq );
-			info = LAPACKE_dlarfb_work ( LAPACK_COL_MAJOR, 'L', 'N', 'F', 'C', ml, n, vb, Aii, lda, Tii, ldt, Qij, ldq, work, ldwork );
+//			info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'A', vb, n, (0e+00), (0e+00), Qij, ldq );
+//			info = LAPACKE_dlarfb_work ( LAPACK_COL_MAJOR, 'L', 'N', 'F', 'C', ml, n, vb, Aii, lda, Tii, ldt, Qij, ldq, work, ldwork );
 
 
 		}
