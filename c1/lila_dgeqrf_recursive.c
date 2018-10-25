fbd73be0d67ad0625ecd1ed9827c36d641454a69
Julien Langou <julien.langou@ucdenver.edu>
2018-10-25 15:34:33 +0800

    edit in recursive code. Nothing major

--- c1/lila_dgeqrf_recursive.c
+++ c1/lila_dgeqrf_recursive.c
@@ -1,6 +1,4 @@
 #include "lila.h"
-#include <math.h>
-
 
 int lila_dgeqrf_recursive( int m, int n, int i, int mt, double *A, int lda, double *T, int ldt, double *TTT, int llldddttt, double *Q, int ldq, double *work, int lwork ){
 
@@ -32,33 +30,10 @@ int lila_dgeqrf_recursive( int m, int n, int i, int mt, double *A, int lda, doub
 	lila_dge_qr_larft_connect_w03( m, nb2, i+nb1, mt, A, lda, TTT, llldddttt );
 
 	info = lila_dge_qr_ormqrbz_w00( m, nb2, nb1, i, i+nb1, mt, A, lda, Q, ldq, T, ldt, work, lwork );
-
-//	This one will need to wait until I get it working completely.
 //	info = lila_dge_qr_ormqrbz_w03( m, nb2, nb1, i, i+nb1, mt, A, lda, Q, ldq, TTT, llldddttt, work, lwork );
-//	info = lila_dge_qr_ormqrbz_w03( m, nb2, nb1, i, i+nb1, mt, A, lda, Q, ldq, T, ldt, work, lwork );
+
 	}
 
-/*
-	printf("\n");
-	int i1, j1;
-	for( i1 = 0; i1 < n ; i1++){
-	for( j1 = 0; j1 < n ; j1++){
-		printf(" %+5.3f", T[ i1 + j1*ldt ] );
-	}
-	printf("\n");
-	}
-	//printf(" %10.7f", T[ 0 ] );
-	printf("\n");
-
-
-	for( i1 = 0; i1 < mt ; i1++){
-	for( j1 = 0; j1 < n ; j1++){
-		printf(" %+5.3f", TTT[ i1 + j1*mt ] );
-	}
-	printf("\n");
-	}
-
-*/
 	return 0;
 
 }
