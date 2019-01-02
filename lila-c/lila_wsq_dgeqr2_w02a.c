d24cec5b3cd431e112b2f51674c894c62727b2f2
Julien Langou <julien.langou@ucdenver.edu>
2019-01-02 16:12:05 -0700

    silencing some warnings

--- lila-c/lila_wsq_dgeqr2_w02a.c
+++ lila-c/lila_wsq_dgeqr2_w02a.c
@@ -3,8 +3,6 @@
 int lila_wsq_dgeqr2_w02a( int m, int n, int i, int mt, double *A, int lda, double *T, int ldt, double *Q, int ldq, double *work ){
 
 	int info ; 
-	double *tau=NULL;
-	double *Aii, *Tii, *Qii;
 	int ml;
 	double w;
 	int lwork, lwork1;
@@ -13,9 +11,9 @@ int lila_wsq_dgeqr2_w02a( int m, int n, int i, int mt, double *A, int lda, doubl
 
 	lwork = 0;
 
-  	info = LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, ml, n, Aii, lda, tau, &w, -1 ); lwork1 = ((int) w); if ( lwork1 > lwork ) lwork = lwork1;
+  	info = LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, ml, n, NULL, lda, NULL, &w, -1 ); lwork1 = ((int) w); if ( lwork1 > lwork ) lwork = lwork1;
 
-	info = LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, ml, n, n, Qii, ldq, tau, &w, -1 ); lwork1 = ((int) w); if ( lwork1 > lwork ) lwork = lwork1;
+	info = LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, ml, n, n, NULL, ldq, NULL, &w, -1 ); lwork1 = ((int) w); if ( lwork1 > lwork ) lwork = lwork1;
 
 	lwork += n;
 
