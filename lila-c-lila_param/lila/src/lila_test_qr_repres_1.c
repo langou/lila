e2484c59fc31d560c7456af0da1ad0eb2f6b48c5
Julien Langou <julien.langou@ucdenver.edu>
2019-03-31 07:47:41 -0600

    minor

--- lila-c-lila_param/lila/src/lila_test_qr_repres_1.c
+++ lila-c-lila_param/lila/src/lila_test_qr_repres_1.c
@@ -20,9 +20,8 @@ double lila_test_qr_repres_1( int m, int n, int i, double *A, int lda, double *Q
 	cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, ml, n, (1.0e+00), Rii, lda, work, ml );
  	for(ii = 0; ii < ml; ii++) for(jj = 0; jj < n; jj++) work[ ii+jj*ml ] -= Aii[ ii+jj*lda ];
 	norm_repres = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', ml, n, work, ml, NULL );
-	free( work );
-
 	norm_repres = norm_repres / normA;
+	free( work );
 
 	return norm_repres;
 }
