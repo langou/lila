f20577f3e9add0552adfb1d5bd86d474d3c63547
Julien Langou <julien.langou@ucdenver.edu>
2019-03-31 07:48:00 -0600

    one bad bug

--- lila-c-lila_param/lila/src/lila_test_qr_repres_1.c
+++ lila-c-lila_param/lila/src/lila_test_qr_repres_1.c
@@ -17,7 +17,7 @@ double lila_test_qr_repres_1( int m, int n, int i, double *A, int lda, double *Q
 	lwork = ml*n;
 	work  = (double *) malloc(ml * n * sizeof(double));
 	info  = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', ml, n, Qii, ldq, work, ml );
-	cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, ml, n, (1.0e+00), Rii, lda, work, ml );
+	cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, ml, n, (1.0e+00), Rii, ldr, work, ml );
  	for(ii = 0; ii < ml; ii++) for(jj = 0; jj < n; jj++) work[ ii+jj*ml ] -= Aii[ ii+jj*lda ];
 	norm_repres = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', ml, n, work, ml, NULL );
 	norm_repres = norm_repres / normA;
