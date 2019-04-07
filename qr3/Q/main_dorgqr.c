82a3331c1f4bbb0467468cd1fa18427ba812182b
Julien Langou <julien.langou@ucdenver.ed>
2019-04-06 21:04:42 -0600

    iconufsinhx

--- qr3/Q/main_dorgqr.c
+++ qr3/Q/main_dorgqr.c
@@ -71,6 +71,7 @@ int main(int argc, char ** argv) {
 	lwork = -1;
 	work  = (double *) malloc( 1 * sizeof(double));
 	info = LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, m, n, A, lda, tau, work, lwork ); 
+	printf("\ninfo = %d\n",info);
 	lwork = (int) work[0];
 	free(work);
 	work  = (double *) malloc( lwork * sizeof(double));
@@ -78,25 +79,36 @@ int main(int argc, char ** argv) {
 	gettimeofday(&tp, NULL);
 	elapsed_ref1=-((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
 	info = LAPACKE_dgeqrf_work( LAPACK_COL_MAJOR, m, n, A, lda, tau, work, lwork ); 
+	printf("\ninfo = %d\n",info);
 	gettimeofday(&tp, NULL);
 	elapsed_ref1+=((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
 	perform_ref1 = ( 2.0e+00 * ((double) m) * ((double) n) * ((double) n) - 2.0e+00 / 3.0e+00 * ((double) n) * ((double) n) * ((double) n) )  / elapsed_ref1 / 1.0e+9 ;
 
 	free( work );
 
-	lwork = nb * n;
-	work  = (double *) malloc( nb * n * sizeof(double));	
+//	lwork = nb * n;
+//	work  = (double *) malloc( nb * n * sizeof(double));	
 
-	info = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'L', m-1, n, A+1, lda, Q+1, ldq );
-	info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'U', n, n, (3.0e+00), (2.0e+00), Q, ldq);
+	lwork = -1;
+	work  = (double *) malloc( 1 * sizeof(double));
+	info = LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, m, n, n, Q, ldq, tau, work, lwork );
+	lwork = (int) work[0];
+	free(work);
+	work  = (double *) malloc( lwork * sizeof(double));
+
+	info = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, n, A, lda, Q, ldq );
+	printf("\ninfo = %d\n",info);
+//	info = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'L', m-1, n, A+1, lda, Q+1, ldq );
+//	info = LAPACKE_dlaset( LAPACK_COL_MAJOR, 'U', n, n, (3.0e+00), (2.0e+00), Q, ldq);
 
 	gettimeofday(&tp, NULL);
 	elapsed_ref2=-((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
 
-//	info = LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, m, n, n, Q, ldq, tau, work, lwork );
+	info = LAPACKE_dorgqr_work( LAPACK_COL_MAJOR, m, n, n, Q, ldq, tau, work, lwork );
+	printf("\ninfo = %d\n",info);
 //	dorgqr_( &m, &n, &n, Q, &ldq, tau, work, &lwork, &info );
 //	our_dorgqr( m, n, n, nb, nb, 0, Q, ldq, tau, work, lwork, info );
-	dV2Q( m, n, n, nb, nb, 0, Q, ldq, tau, work, lwork, info );
+//	dV2Q( m, n, n, nb, nb, 0, Q, ldq, tau, work, lwork, info );
 
 	gettimeofday(&tp, NULL);
 	elapsed_ref2+=((double)tp.tv_sec+(1.e-6)*tp.tv_usec);
@@ -126,6 +138,10 @@ int main(int argc, char ** argv) {
 
 		info = qr3_test_qq_orth_1( &orth, m, n, Q, ldq );		
 		if ( verbose ) printf("qq_orth  = %5.1e  \n ",orth); else printf(" %5.1e  ",orth); 
+
+	printf("\n||A|| = %f\n", LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, n, As, lda, NULL ) );
+	printf("%f\n",As[0]);
+
 
 		info = qr3_test_qr_repres_1( &repres_1, m, n, As, lda, Q, ldq, A, lda );
 		if ( verbose ) printf("qr_repres = %5.1e  \n ",repres_1); else printf(" %5.1e  ",repres_1); 
--- qr3/Q/qr3_test_qr_repres_1.c
+++ qr3/Q/qr3_test_qr_repres_1.c
@@ -6,11 +6,16 @@ int qr3_test_qr_repres_1( double *norm_repres, int m, int n, double *A, int lda,
 	int info, lwork, jj, ii;
 
 	normA = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, n, A, lda, NULL );
+	printf("\n||A|| = %f\n",normA);
 
 	lwork = m*n;
 	work  = (double *) malloc(m * n * sizeof(double));
 	info  = LAPACKE_dlacpy_work( LAPACK_COL_MAJOR, 'A', m, n, Q, ldq, work, m );
 	cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, m, n, (1.0e+00), R, ldr, work, m );
+	cblas_dtrmm( CblasColMajor, CblasRight, CblasUpper, CblasNoTrans, CblasNonUnit, m, n, (1.0e+00), R, ldr, Q, ldq );
+	printf("\n%f   ",Q[0]);
+	printf("\n%f   ",work[0]);
+	printf("%f\n",A[0]);
  	for(ii = 0; ii < m; ii++) for(jj = 0; jj < n; jj++) work[ ii+jj*m ] -= A[ ii+jj*lda ];
 	(*norm_repres) = LAPACKE_dlange_work( LAPACK_COL_MAJOR, 'F', m, n, work, m, NULL );
 	free( work );
