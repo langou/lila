6af13f84465650a7fb59d48a2100a54651b03116
Julien Langou <julien.langou@ucdenver.edu>
2019-05-09 05:42:27 -0600

    pif

--- qr3/Q/flops_lapack_org2r.c
+++ qr3/Q/flops_lapack_org2r.c
@@ -17,14 +17,60 @@ long int flops_lapack_org2r( int int_m, int int_n, int int_k ){
 	n = ( long int ) int_n;
 	k = ( long int ) int_k;
 	
-	flops = ( (( long int ) 12 ) * m * n * k
-		- (( long int )  6 ) * m * k * k
-		- (( long int )  6 ) * n * k * k
-		+ (( long int )  4 ) * k * k * k
-		- (( long int )  3 ) * m * k
-		+ (( long int )  3 ) * n * k
-		- (( long int )  1 ) * k )
+//	flops = ( (( long int ) 12 ) * m * n * k
+//		- (( long int )  6 ) * m * k * k
+//		- (( long int )  6 ) * n * k * k
+//		+ (( long int )  4 ) * k * k * k
+//		- (( long int )  3 ) * m * k
+//		+ (( long int )  3 ) * n * k
+//		- (( long int )  1 ) * k )
+//		/ (( long int )  3 );
+
+//	flops = ( (( long int ) 12 ) * m * n * k
+//		- (( long int )  6 ) * m * k * k
+//		- (( long int )  6 ) * n * k * k
+//		+ (( long int )  4 ) * k * k * k
+//		- (( long int ) 12 ) * m * n
+//		+ (( long int )  9 ) * m * k
+//		+ (( long int ) 15 ) * n * k
+//		- (( long int ) 12 ) * k * k
+//		- (( long int )  9 ) * n
+//		+ (( long int )  8 ) * k )
+//		/ (( long int )  3 );
+//	flops += 4 * (m-k+1)  * (n-k) - (n-k) ;
+
+
+//      this is to create Q1
+//	flops = ( 
+//	        + 6 * (m-k) * k * k
+//	        + 4 * k * k * k
+//		- 3 * (m-k) * k
+//		- 1 * k
+//		)
+//		/ (( long int )  3 );
+//
+//      this is the ``BZ`` on Q2
+//	flops +=  4 * m * (n-k) * k
+//		- 2 * (n-k) * k * k
+//		- 4 * m * (n-k)
+//		- 3 * (n-k)
+//		+ 5 * (n-k) * k ;
+//
+//      this is the LARF to create Q2
+//	flops += 4 * (m-k+1)  * (n-k) - (n-k) ;
+ 
+//      this is the cost for creating Q2
+	flops = ( 
+	        + 6 * (m-k) * k
+	        + 4 * k * k
+		- 3 * (m-k)
+		- 1 )
+		* k
 		/ (( long int )  3 );
+
+//      this is the cost for creating Q2
+	flops +=  ( 4 * m - 2 * k + 1 ) * k * ( n - k ) ;
+
 
 	return flops;
 
