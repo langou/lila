2f4d8b211fec5782f3a8c8b461b89708a9acb845
Daniel Bielich <daniel.bielich@ucdenver.edu>
2019-05-28 11:19:47 -0600

    Clean up

--- qr3/Q/wrapper_dlarf.c
+++
@@ -1,9 +0,0 @@
-#include "qr3.h"
-
-int wrapper_dlarf( char side, int m, int n, double *V, int incv, double tau, double *C, int ldc, double *work){
-
-	dlarf_( &side, &m, &n, V, &incv, &tau, C, &ldc, work);
-
-
-	return 0;
-}
